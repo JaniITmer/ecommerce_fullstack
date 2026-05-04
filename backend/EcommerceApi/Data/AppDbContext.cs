78b4bef2efd23fe9c63227d05a7a14f2a16fc1e7
Nagy János <batyus4563@gmail.com>
2026-05-04 13:16:59 +0200

    feat:  AppDbContext and initial database migration

---
+++ backend/EcommerceApi/Data/AppDbContext.cs
@@ -0,0 +1,37 @@
+﻿using EcommerceApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceApi.Data
+{
+    public class AppDbContext:DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasPrecision(12,2);
+
+            modelBuilder.Entity<OrderItem>()
+                .Property(oi => oi.UnitPrice)
+                .HasPrecision(14,2);
+
+            modelBuilder.Entity<Order>()
+                .Property(o => o.TotalAmount)
+                .HasPrecision(16,2);
+        }
+    }
+
+   
+    }
