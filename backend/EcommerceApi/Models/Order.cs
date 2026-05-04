3ffc088e9a395ec21a855e95ed099a9eb1b34924
Nagy János <batyus4563@gmail.com>
2026-05-04 12:30:05 +0200

    adding Models for backend

---
+++ backend/EcommerceApi/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿namespace EcommerceApi.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+        public string Status { get; set; } = "Pending";
+        public decimal TotalAmount { get; set; }
+        public int UserId { get; set; }
+        public User? User { get; set; }
+
+        public ICollection<OrderItem>OrderItems { get; set; }= new List<OrderItem>();
+
+
+    }
+}
---
+++ backend/EcommerceApi/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace EcommerceApi.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public string Role { get; set; } = "Customer"; 
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public ICollection<Order> Orders { get; set; } = new List<Order>();
+    }
+}
